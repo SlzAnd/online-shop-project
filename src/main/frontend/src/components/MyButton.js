5e3ba7bd9fc1d9e37209db148cd3e231c171071f
Andrii Seleznov <seleznov.andriy@gmail.com>
2023-05-06 16:18:24 +0300

    created button for calling "add product form"

---
+++ src/main/frontend/src/components/MyButton.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+function MyButton(props) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div>
+            <button className={"link-btn"} onClick={handleClick}>Add new product</button>
+            <div className={"modal"}>
+                <div className={"modal-content"}>
+                    <Modal  isOpen={isModalOpen} onRequestClose={handleCloseModal}>
+                        {props.formComponent && (
+                            <props.formComponent onClose={handleCloseModal} />
+                        )}
+                    </Modal>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MyButton;
