ccb53c0fa412717ab901265dc743f4e976bbf808
Andrii Seleznov <seleznov.andriy@gmail.com>
2023-05-06 16:16:34 +0300

    Created component with all products(axios get request)

---
+++ src/main/frontend/src/components/ProductList.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect, useCallback} from "react";
+import axios from "axios";
+
+const ProductList = () => {
+
+    const [productList, setProductList] = useState([]);
+    const fetchProductList = () => {
+        axios.get("http://localhost:8080/api/products").then(res => {
+            console.log(res);
+            setProductList(res.data);
+        })
+    }
+
+    useEffect(() => {
+        fetchProductList()
+    }, [])
+
+    return productList.map((productList, index) => {
+        return (
+            <div className={"productCard"}>
+                <h2>{productList.name}</h2>
+                <h3>Price: {productList.price} uah</h3>
+                <h3>Quantity: {productList.quantity}</h3>
+                <p>Description: {productList.description}</p>
+            </div>
+        )
+    })
+}
+
+export default ProductList;
