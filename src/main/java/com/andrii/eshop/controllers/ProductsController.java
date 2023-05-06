740e10f3540526d244c1a3fe47ccb3ecd0dfbb7f
Andrii Seleznov <seleznov.andriy@gmail.com>
2023-05-06 16:07:56 +0300

    Deleted unused import statements

--- src/main/java/com/andrii/eshop/controllers/ProductsController.java
+++ src/main/java/com/andrii/eshop/controllers/ProductsController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
