d51c60fb2fcb4b003837cc83e8998667e5f92719
Andrii Seleznov <seleznov.andriy@gmail.com>
2023-06-06 10:32:52 +0300

    created search by name functionality

--- src/main/java/com/andrii/eshop/repositories/ProductRepository.java
+++ src/main/java/com/andrii/eshop/repositories/ProductRepository.java
@@ -1,7 +1,13 @@
 package com.andrii.eshop.repositories;
 
 import com.andrii.eshop.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findProductsByNameContainingIgnoreCase (String searchQuery, Pageable pageable);
 }
